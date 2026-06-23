fbb2217b12ce90a17dd82eac87b16c4de582d7fc
FernandoGabrielSalguero <149271548+FernandoGabrielSalguero@users.noreply.github.com>
2026-06-23 20:13:07 -0300

    main

--- contacto.php
+++ contacto.php
@@ -146,6 +146,7 @@
           </div>
         </div>
         <form class="contact-form reveal" data-mail-form data-form-context="contacto" novalidate>
+          <h2 class="contact-form-mobile-title">Conversemos sobre tu crecimiento en Argentina</h2>
           <div class="field-grid">
             <label>
               Nombre y Apellido
